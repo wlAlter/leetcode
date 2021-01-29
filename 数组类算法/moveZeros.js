b362ecb78fdce4f75edb356c81776bc8f5443bc7
wlin <wlin930@outlook.com>
2021-01-29 18:14:22 +0800

    format

--- 数组类算法/moveZeros.js
+++ 数组类算法/moveZeros.js
@@ -7,13 +7,6 @@ var moveZeroes = function (nums) {
   let prev = 0,
     next = 1;
   while (next < nums.length) {
-    // 第一次提交, 部分通过. 纸上设计时, 过早收束了switch条件
-    // if (nums[prev] == 0 && nums[next] != 0) {
-    //   nums[prev] = nums[next];
-    //   nums[next] = 0;
-    //   prev += 1;
-    // }
-    // next++;
     if (nums[prev] == 0) {
       if (nums[next] == 0) {
         next++;
