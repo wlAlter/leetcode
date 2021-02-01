dd41b7ef599fbeb517fb7f46afbcd9ac6ef19356
wlin <wlin930@outlook.com>
2021-02-01 14:59:44 +0800

    200: 提交通过

--- 数组类算法/removeElement.js
+++ 数组类算法/removeElement.js
@@ -4,20 +4,17 @@
  * @return {number}
  */
 var removeElement = function (nums, val) {
-  for (let i = 0; i < nums.length; i++) {
-    const element = nums[i];
-    if (element == val) {
-      nums.splice(i, 1);
+  let index = 0;
+  while (index < nums.length) {
+    if (nums[index] == val) {
+      nums.splice(index, 1);
+    } else {
+      index++;
     }
   }
   return nums.length;
 };
-let nums = [3, 2, 2, 3];
+let nums = [0, 1, 2, 2, 3, 0, 4, 2];
 
-console.log(removeElement(nums, 3));
+console.log(removeElement(nums, 2));
 console.log(nums);
-// 85/113个通过测试用例
-// input [0,1,2,2,3,0,4,2]
-// output [0,1,2,3,0,4]
-// 预期 [0,1,4,0,3]
-// 忽略了删除元素后, array下标前移
