7bc14d7d588bedcd5147af1dd65b8600e494c269
wlin <wlin930@outlook.com>
2021-02-01 16:54:19 +0800

    200; perf; 双指针, 有变化, prev才偏移

--- 数组类算法/removeDuplicates.js
+++ 数组类算法/removeDuplicates.js
@@ -3,15 +3,16 @@
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] == nums[j]) {
-        nums.splice(j, 1);
-        j--;
-      }
+  let prev = 0,
+    next = 0;
+  while (next < nums.length) {
+    if (nums[prev] != nums[next]) {
+      prev += 1;
+      nums[prev] = nums[next];
     }
+    next++;
   }
-  return nums.length;
+  return prev + 1;
 };
 
 let nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
